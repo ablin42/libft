ed8e5e20f8dc6444456e819f0426caef409a5339
Andreas BLIN <ablin@e1r1p5.42.fr>
2017-12-03 22:04:43 +0100

    need bonus

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,80 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablin <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/03 21:41:19 by ablin             #+#    #+#             */
+/*   Updated: 2017/12/03 21:43:22 by ablin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <stdio.h>
 
-int		ft_nb_words(char const *s, char c)
+static int	ft_nb_words(char const *s, char c)
 {
-	int i;
 	int		nb;
+	int		i;
 
-	i = 0;
 	nb = 0;
-	if (s[0] != c)
-		nb++;
+	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i - 1] == c && s[i] != c)
+		if (s[i] != c)
+		{
 			nb++;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+			i--;
+		}
 		i++;
 	}
 	return (nb);
 }
 
-int		ft_word_length(char const *s, char c, int start)
+static int	ft_word_length(char const *s, char c)
 {
-	int len;
+	int		len;
 
 	len = 0;
-	while (s[start] != c && s[start] != '\0')
+	while (*s != c && *s != '\0')
 	{
-		start++;
+		s++;
 		len++;
 	}
 	return (len);
 }
 
-char	**ft_strsplit(char const *s, char c)
+char		**ft_strsplit(char const *s, char c)
 {
 	int		i;
-	int		j;
-	int		tmp;
-	int		start;
-	int		k;
+	int		nb_word;
 	char	**split;
 
 	i = 0;
-	j = 0;
-	k = 0;
-	start = 0;
-	tmp = ft_nb_words(s, c);
-	if ((split = (char **)malloc(sizeof(*split) * (tmp + 1))) == NULL)
+	nb_word = ft_nb_words(s, c);
+	if ((split = (char **)malloc(sizeof(*split) * (nb_word + 1))) == NULL)
 		return (NULL);
-
-	while (s[i] != '\0')
+	while (nb_word--)
 	{
-		k = 0;
-
-		while (s[i] == c)
-		{
-			start = i + 1;
-			i++;
-		}
-
-
-			tmp = ft_word_length(s, c, start);
-			if ((split[j] = (char *)malloc(sizeof(**split) * (tmp + 1))) == NULL)
-				return (NULL);
-			split[j][tmp] = '\0';
-			while (k < tmp)
-			{
-				split[j][k] = s[i + k];
-				k++;
-			}
-		j++;
-		k--;
-		
-
-		i = i + k;
+		while (*s == c && *s != '\0')
+			s++;
+		split[i] = ft_strsub(s, 0, ft_word_length(s, c));
+		if (split[i] == NULL)
+			return (NULL);
+		s = s + ft_word_length(s, c);
 		i++;
 	}
+	split[i] = NULL;
 	return (split);
 }
